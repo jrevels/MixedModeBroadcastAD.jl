d0d88a1116e2f94a6bd0fb7666785dacddc04ff4
Tim Besard <tim.besard@gmail.com>
2018-01-11 12:58:14 +0100

    not needed

--- test/runtests.jl
+++ test/runtests.jl
@@ -89,7 +89,7 @@ output, grads = autograd(tape, example, input...)
 # LSTM-like kernel with CuArray #
 #################################
 
-@inline function cu_example(a1, a2, b1, b2, c1, c2, d1, d2)
+function cu_example(a1, a2, b1, b2, c1, c2, d1, d2)
     a = a1 * a2
     b = b1 * b2
     c = c1 * c2
