1ab9e6e798d47c97004ec214869a5b04da9a5356
Tim Besard <tim.besard@gmail.com>
2018-01-24 09:55:04 +0100

    Fix benchmarks.

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,5 +1,5 @@
-using MixedModeBroadcastAD: record, forward!, backward!
-
+using MixedModeBroadcastAD: record, forward!, backward!, CuArray, lstm_update_c, cuda_lstm_update_c
+import CUDAdrv
 #=
 Kernels benchmarked here are implemented in `src/kernels` and tested for correctness
 in `test/runtests.jl`.
@@ -9,21 +9,22 @@ benchmarking code below pre-records the tape in order to avoid timing irrelevant
 overhead due to tape construction.
 =#
 
-function benchmark_lstm_update_c(n::Int, usegpu::Bool)
-    f = usegpu ? MixedModeBroadcastAD.cuda_lstm_update_c : MixedModeBroadcastAD.lstm_update_c
-    c = rand(n)
-    Wxs = (rand(n) for _ in 1:3)
-    Rhs = (rand(n) for _ in 1:3)
-    bs  = (rand(n) for _ in 1:3)
-    tape, _, _ = record(f, c, Wxs..., Rhs..., bs...)
-    println("---------------------------------------------------------")
-    println("using GPU: ", usegpu)
-    println("size: ", n)
-    println("forward pass time: ", @elapsed(forward!(tape)), " seconds")
-    println("backward pass time: ", @elapsed(backward!(tape)), " seconds")
+function benchmark(::Type{Array}, n::Int)
+    input = Tuple(rand(Float32, n, n) for i in 1:10)
+    tape, _, _ = record(lstm_update_c, input...)
+    @elapsed(forward!(tape)), @elapsed(backward!(tape))
 end
 
-for n in (2^i for i in 9:11)
-    benchmark(n, false)
-    benchmark(n, true)
+function benchmark(::Type{CuArray}, n::Int)
+    input = Tuple(rand(Float32, n, n) for i in 1:10)
+    tape, _, _ = record(cuda_lstm_update_c, CuArray.(input)...)
+    CUDAdrv.@elapsed(forward!(tape)), CUDAdrv.@elapsed(backward!(tape))
 end
+
+rows = Any[["size", "type", "forwards", "backwards"]]
+for n in (2^i for i in 9:11), T in [Array, CuArray]
+    benchmark(T, n) # warm-up
+    fwd, bwd = benchmark(T, n)
+    push!(rows, [T, "$(n)x$(n)", "$fwd s", "$bwd s"])
+end
+show(STDOUT, Markdown.MD(Markdown.Table(rows, [:r, :c, :c, :c])))
