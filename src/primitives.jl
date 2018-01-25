6b8fe8178b482601e5c1fe8d7a11e6e08e847cac
Jarrett Revels <jarrettrevels@gmail.com>
2018-01-25 14:10:29 -0500

    add required machinery for AD of unfused benchmark

--- src/primitives.jl
+++ src/primitives.jl
@@ -1,10 +1,22 @@
+#############################
+# non-stdlib math functions #
+#############################
+
+σ(x) = 1 / (1 + exp(-x))
+d_σ(x) = (expx = exp(x); expx / (1 + expx)^2)
+
+cuda_σ(x) = 1 / (1 + CUDAnative.exp(-x))
+d_cuda_σ(x) = (expx = CUDAnative.exp(x); expx / (1 + expx)^2)
+
+d_tanh(x) = sech(x)^2
+
 ###############
 # Record Pass #
 ###############
 
 #=== auto-defined methods ===#
 
-const FORWARD_METHODS = [(:(Base.sum), 1), (:(Base.:*), 2)]
+const FORWARD_METHODS = [(:(Base.sum), 1), (:(Base.:*), 2), (:(Base.:+), 2)]
 
 for (f, arity) in FORWARD_METHODS
     if arity == 1
@@ -39,12 +51,26 @@ end
 # Forward Pass #
 ################
 
+#=== generic fallback ===#
+
 function forward!(i::Instruction)
     i.output.value = i.func(map(value, i.input)...)
     return nothing
 end
 
-function forward!(i::Instruction{typeof(broadcast)})
+#=== broadcast fallbacks ===#
+
+# multiple dispatch selects these implementations for the unfused benchmarks
+
+forward!(i::BroadcastInstruction{typeof(σ)}) = invoke(forward!, Instruction, i)
+forward!(i::BroadcastInstruction{typeof(cuda_σ)}) = invoke(forward!, Instruction, i)
+forward!(i::BroadcastInstruction{typeof(tanh)}) = invoke(forward!, Instruction, i)
+
+#=== mixed-mode broadcast optimization ===#
+
+# multiple dispatch selects this implementation for fused benchmarks
+
+function forward!(i::BroadcastInstruction)
     output_variable = isa(i.output, Variable) ? i.output : first(i.output)
     f, values = first(i.input), value.(i.input[2:end])
     N, T = length(values), eltype(value(output_variable))
@@ -73,23 +99,7 @@ end
 # Backwards Pass #
 ##################
 
-# FIXME: this `@inbounds` is incorrectly placed, but it doesn't seem to have any impact
-#        when placed at the `getpartial` call site
-Base.@propagate_inbounds getpartial(x, i) = @inbounds DiffResults.derivative(x)[i]
-
-# This broadcast `backward!` implementation is actually incomplete, but it doesn't matter
-# for our performance experiment. Specifically, it doesn't implement the proper reduction
-# and expansion semantics encountered when the arguments have different shapes. In other
-# words, this implementation only works when all broadcast arguments are arrays of the same
-# shape (which is what we're benchmarking anyway).
-function backward!(i::Instruction{typeof(broadcast)})
-    f, args = first(i.input), i.input[2:end]
-    output, df_results = i.output
-    for i in 1:length(args)
-        @propagate!(args[i], getpartial.(df_results, i) .* deriv(output))
-    end
-    return nothing
-end
+#=== standard reverse definitions ===#
 
 function backward!(i::Instruction{typeof(sum)})
     x = first(i.input)
@@ -103,5 +113,59 @@ function backward!(i::Instruction{typeof(*)})
     z = i.output
     @propagate!(x, deriv(z) * value(y)')
     @propagate!(y, value(x)' * deriv(z))
+    return nothing
+end
+
+function backward!(i::Instruction{typeof(+)})
+    x, y = i.input
+    z = i.output
+    @propagate!(x, deriv(z))
+    @propagate!(y, deriv(z))
+    return nothing
+end
+
+for (f, df) in [(:σ, :d_σ), (:cuda_σ, :d_cuda_σ), (:tanh, :d_tanh)]
+    @eval function backward!(i::BroadcastInstruction{typeof($f)})
+        f, args = first(i.input), i.input[2:end]
+        for i in 1:length(args)
+            @propagate!(args[i], $(df).(value(args[i])) .* deriv(i.output))
+        end
+        return nothing
+    end
+end
+
+function backward!(i::BroadcastInstruction{typeof(*)})
+    x, y = i.input
+    z = i.output
+    @propagate!(x, value(y) .* deriv(z))
+    @propagate!(y, value(x) .* deriv(z))
+    return nothing
+end
+
+function backward!(i::BroadcastInstruction{typeof(+)})
+    x, y = i.input
+    z = i.output
+    @propagate!(x, deriv(z))
+    @propagate!(y, deriv(z))
+    return nothing
+end
+
+#=== mixed-mode broadcast optimization ===#
+
+# FIXME: this `@inbounds` is incorrectly placed, but it doesn't seem to have any impact
+#        when placed at the `getpartial` call site
+Base.@propagate_inbounds getpartial(x, i) = @inbounds DiffResults.derivative(x)[i]
+
+# This broadcast `backward!` implementation is actually incomplete, but it doesn't matter
+# for our performance experiment. Specifically, it doesn't implement the proper reduction
+# and expansion semantics encountered when the arguments have different shapes. In other
+# words, this implementation only works when all broadcast arguments are arrays of the same
+# shape (which is sufficient for our benchmarking purposes).
+function backward!(i::BroadcastInstruction)
+    f, args = first(i.input), i.input[2:end]
+    output, df_results = i.output
+    for i in 1:length(args)
+        @propagate!(args[i], getpartial.(df_results, i) .* deriv(output))
+    end
     return nothing
 end
--- test/kernels.jl
+++ test/kernels.jl
@@ -1,22 +1,15 @@
-#=
-Since our technique only affects broadcast performance, we have limited our kernel to
-execute only a small part of the LSTM layer which stresses fused broadcast operations. This
-allows us tobenchmark/test our technique in isolation from the other parts of our ad-hoc
-AD framework which are not necessarily performant and only exist to facilitate the
-demonstration of the broadcast technique.
-=#
-
+using MixedModeBroadcastAD: σ, cuda_σ
 import CUDAdrv
-
-
-#
-# Primitives
-#
 
 const cuda_lib = Libdl.dlopen(joinpath(@__DIR__, "kernels.so"))
 
-σ(x) = 1 / (1 + exp(-x))
-cuda_σ(x) = 1 / (1 + CUDAnative.exp(-x))
+#=
+Since our technique only affects broadcast performance, we have limited our kernel to
+execute only a small part of the LSTM layer which stresses fused broadcast operations. This
+allows us to benchmark/test our technique in isolation from the other parts of our ad-hoc AD
+framework which are not necessarily performant and only exist to facilitate the
+demonstration of the broadcast technique.
+=#
 
 
 #
@@ -62,67 +55,36 @@ end
 # Unfused
 #
 
-function unfused_lstm_update_c(tmp1, tmp2, c,
+#=
+Manually call `broadcast` instead of using dot syntax to avoid Julia's automatic syntatic
+fusion. This is exactly what would be called in the above kernels if Julia didn't perform
+syntatic fusion automatically.
+=#
+
+function unfused_lstm_update_c(c,
                                Wx_f, Wx_i, Wx_c,
                                Rh_f, Rh_i, Rh_c,
                                b_f,  b_i,  b_c)
-    out = similar(c)
-
-    # σ.(Wx_f .+ Rh_f .+ b_f) .* c
-    tmp1 .= Wx_f .+ Rh_f
-    tmp1 .= tmp1 .+ b_f
-    tmp1 .= σ.(tmp1)
-    tmp1 .= tmp1 .* c
-
-    # σ.(Wx_i .+ Rh_i .+ b_i)
-    tmp2 = Wx_i .+ Rh_i
-    tmp2 .= tmp2 .+ b_i
-    tmp2 .= σ.(tmp2)
-
-    # tanh.(Wx_c .+ Rh_c .+ b_c)
-    out .= Wx_c .+ Rh_c
-    out .= out .+ b_c
-    out .= tanh.(out)
-
-    # σ.(...) * tanh.(...)
-    out .= out .* tmp2
-
-    # σ.(...) + σ.(...) * tanh.(...)
-    out .= out .+ tmp1
-
-    return out
+    return broadcast(+,
+                     broadcast(*,
+                               broadcast(σ, broadcast(+, Wx_f, Rh_f, b_f)),
+                               c),
+                     broadcast(*,
+                               broadcast(σ,    broadcast(+, Wx_i, Rh_i, b_i)),
+                               broadcast(tanh, broadcast(+, Wx_c, Rh_c, b_c))))
 end
 
-function unfused_cudanative_lstm_update_c(tmp1, tmp2, c,
+function unfused_cudanative_lstm_update_c(c,
                                           Wx_f, Wx_i, Wx_c,
                                           Rh_f, Rh_i, Rh_c,
                                           b_f,  b_i,  b_c)
-    out = similar(c)
-
-    # σ.(Wx_f .+ Rh_f .+ b_f) .* c
-    tmp1 .= Wx_f .+ Rh_f
-    tmp1 .= tmp1 .+ b_f
-    tmp1 .= σ.(tmp1)
-    tmp1 .= tmp1 .* c
-
-    # σ.(Wx_i .+ Rh_i .+ b_i)
-    tmp2 = Wx_i .+ Rh_i
-    tmp2 .= tmp2 .+ b_i
-    tmp2 .= cuda_σ.(tmp2)
-
-    # tanh.(Wx_c .+ Rh_c .+ b_c)
-    out .= Wx_c .+ Rh_c
-    out .= out .+ b_c
-    out .= CUDAnative.tanh.(out)
-
-    # σ.(...) * tanh.(...)
-    out .= out .* tmp2
-
-    # σ.(...) + σ.(...) * tanh.(...)
-    out .= out .+ tmp1
-
-    CUDAdrv.synchronize()
-    return out
+    return broadcast(+,
+                     broadcast(*,
+                               broadcast(cuda_σ, broadcast(+, Wx_f, Rh_f, b_f)),
+                               c),
+                     broadcast(*,
+                               broadcast(cuda_σ, broadcast(+, Wx_i, Rh_i, b_i)),
+                               broadcast(CUDAnative.tanh, broadcast(+, Wx_c, Rh_c, b_c))))
 end
 
 const cuda_fun_unfused = Libdl.dlsym(cuda_lib, "unfused_lstm_update_c")
