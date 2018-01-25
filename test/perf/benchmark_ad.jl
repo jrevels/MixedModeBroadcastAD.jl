a11406fab6a5db232ba85f7b44c8a39995883e2e
Tim Besard <tim.besard@gmail.com>
2018-01-25 13:11:26 +0100

    Invoke AD benchmark with unfused kernels.

--- test/perf/benchmark_ad.jl
+++ test/perf/benchmark_ad.jl
@@ -4,12 +4,17 @@ import CUDAdrv
 include("util.jl")
 include("../kernels.jl")
 
-kernel(::Type{Array}) = lstm_update_c
-kernel(::Type{CuArray}) = cuda_lstm_update_c
+kernel(::Type{Array}, fused) = fused ? lstm_update_c : unfused_lstm_update_c
+kernel(::Type{CuArray}, fused) = fused ? cuda_lstm_update_c : unfused_cuda_lstm_update_c
 
-function prepare(::Type{T}, n::Int) where {T<:AbstractArray}
+function prepare(::Type{T}, n::Int, fused) where {T<:AbstractArray}
     input = Tuple(T{Float32}((n,n)) for i in 1:10)
-    tape, _, _ = record(kernel(T), input...)
+    output = T{Float32}((n,n))
+    tape, _, _ = if fused
+        record(kernel(T, fused), input...)
+    else
+        record(kernel(T, fused), output, input...)
+    end
     tape
 end
 
@@ -22,11 +27,11 @@ function benchmark(::Type{CuArray}, tape)
     @elapsed((backward!(tape), CUDAdrv.synchronize()))
 end
 
-rows = Any[["environment", "size", "forwards", "backwards"]]
-for n in (2^i for i in 9:11), T in [Array, CuArray]
-    tape = prepare(T, n)
+rows = Any[["environment", "size", "fused", "forwards", "backwards"]]
+for fused in [true, false], n in (2^i for i in 9:11), T in [Array, CuArray]
+    tape = prepare(T, n, fused)
     benchmark(T, tape) # warm-up
     fwd, bwd = benchmark(T, tape)
-    push!(rows, ["Julia $T", "$(n)x$(n)", timedelta(fwd), timedelta(bwd)])
+    push!(rows, ["Julia $T", "$(n)x$(n)", fused, timedelta(fwd), timedelta(bwd)])
 end
-println(Markdown.MD(Markdown.Table(rows, [:r, :c, :c, :c])))
+println(Markdown.MD(Markdown.Table(rows, [:r, :c, :c, :c, :c])))
