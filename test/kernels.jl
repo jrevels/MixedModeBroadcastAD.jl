711405a92b56ae9d221579adab54d02aa761ee7a
Tim Besard <tim.besard@gmail.com>
2018-01-25 16:12:51 +0100

    Fix wrong GEMM, should all be elwise ops.

--- test/kernels.jl
+++ test/kernels.jl
@@ -35,25 +35,25 @@ function unfused_lstm_update_c(out, c,
                                b_f,  b_i,  b_c)
     # σ.(Wx_f .+ Rh_f .+ b_f) .* c
     tmp1 = Wx_f .+ Rh_f
-    tmp1 += b_f
+    tmp1 .= tmp1 .+ b_f
     tmp1 .= σ.(tmp1)
-    tmp1 += c
+    tmp1 .= tmp1 .* c
 
     # σ.(Wx_i .+ Rh_i .+ b_i)
     tmp2 = Wx_i .+ Rh_i
-    tmp2 += b_i
+    tmp2 .= tmp2 .+ b_i
     tmp2 .= σ.(tmp2)
 
     # tanh.(Wx_c .+ Rh_c .+ b_c)
     out .= Wx_c .+ Rh_c
-    out += b_c
+    out .= out .+ b_c
     out .= tanh.(out)
 
     # σ.(...) * tanh.(...)
-    out *= tmp2
+    out .= out .* tmp2
 
     # σ.(...) + σ.(...) * tanh.(...)
-    out += tmp1
+    out = out .+ tmp1
 
     return
 end
@@ -64,25 +64,25 @@ function unfused_cuda_lstm_update_c(out, c,
                                     b_f,  b_i,  b_c)
     # σ.(Wx_f .+ Rh_f .+ b_f) .* c
     tmp1 = Wx_f .+ Rh_f
-    tmp1 += b_f
-    tmp1 .= cuda_σ.(tmp1)
-    tmp1 += c
+    tmp1 .= tmp1 .+ b_f
+    tmp1 .= σ.(tmp1)
+    tmp1 .= tmp1 .* c
 
     # σ.(Wx_i .+ Rh_i .+ b_i)
     tmp2 = Wx_i .+ Rh_i
-    tmp2 += b_i
+    tmp2 .= tmp2 .+ b_i
     tmp2 .= cuda_σ.(tmp2)
 
     # tanh.(Wx_c .+ Rh_c .+ b_c)
     out .= Wx_c .+ Rh_c
-    out += b_c
+    out .= out .+ b_c
     out .= CUDAnative.tanh.(out)
 
     # σ.(...) * tanh.(...)
-    out *= tmp2
+    out .= out .* tmp2
 
     # σ.(...) + σ.(...) * tanh.(...)
-    out += tmp1
+    out = out .+ tmp1
 
     return
 end
