82bea0ee2a1cd0f95daa11905271002fdcfd6e8f
Tim Besard <tim.besard@gmail.com>
2018-01-26 13:55:14 +0100

    Add script for profiling w/o AD.

---
+++ test/perf/profile.jl
@@ -0,0 +1,23 @@
+using MixedModeBroadcastAD: record, forward!, backward!, CuArray
+using CUDAnative
+import CUDAdrv
+import NVTX
+include("../kernels.jl")
+
+# NOTE: use with `--profile-from-start off`
+NVTX.stop()
+
+const n = 2048
+
+inputs = Tuple(CuArray{Float32}((n,n)) for i in 1:10)
+
+# warm-up
+cuda_lstm_update_c(inputs...)
+cudanative_lstm_update_c(inputs...)
+
+NVTX.@activate CUDAdrv.@profile begin
+    ccall(:jl_dump_compiles, Void, (Ptr{Nothing},), STDERR.handle)
+    NVTX.@range "CUDA" cuda_lstm_update_c(inputs...)
+    NVTX.@range "CuArray" cudanative_lstm_update_c(inputs...)
+    ccall(:jl_dump_compiles, Void, (Ptr{Void},), C_NULL)
+end
