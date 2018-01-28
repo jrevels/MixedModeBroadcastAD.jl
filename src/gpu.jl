e25cb14505937b6ab5827fc8f9e1ce4da3397af3
Valentin Churavy <v.churavy@gmail.com>
2018-01-27 22:08:01 -0500

    how!? did this work before

--- src/gpu.jl
+++ src/gpu.jl
@@ -179,7 +179,7 @@ end
             @nexprs $N i->(iv_{i} = input_values[i][I])
             ivs = @ncall $N SVector iv 
             ij_result = dual_eval_kernel(kernel, ivs)
-            @inbounds output_value[i] = ForwardDiff.value(ij_result)
+            @inbounds output_value[I] = ForwardDiff.value(ij_result)
             iderivs = view(input_derivs, I, :)
             for k in 1:$N
                 @inbounds iderivs[k] = ForwardDiff.partials(ij_result, k)
