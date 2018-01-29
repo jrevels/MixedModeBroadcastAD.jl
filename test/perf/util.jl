8ddf887d89cb9d23ef63342401176e33a4be101b
Jarrett Revels <jarrettrevels@gmail.com>
2018-01-29 17:50:44 -0500

    fix typo

--- test/perf/util.jl
+++ test/perf/util.jl
@@ -2,7 +2,7 @@ using MixedModeBroadcastAD: record
 using Printf
 include("../kernels.jl")
 
-gettape(args...) = first(record(getkernel(args...)....))
+gettape(args...) = first(record(getkernel(args...)...))
 
 function timedelta(s)
     (unit, factor) = if s < 1e-6
