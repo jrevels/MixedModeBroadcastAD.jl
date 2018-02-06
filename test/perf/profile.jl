efb8031d375b4ef33d836249f8b7d42f0ef9f3e0
Jarrett Revels <jarrettrevels@gmail.com>
2018-02-06 15:55:24 -0500

    fix typo

--- test/perf/profile.jl
+++ test/perf/profile.jl
@@ -14,14 +14,14 @@ const SOA         = length(ARGS) >= 3 ? parse(Bool, ARGS[3]) : true
 const TAPE        = gettape(:gpu, PRECOMPUTED, SOA, DIMS)
 
 function benchmark(tape)
-    NVTX.@range "forward pass"  (forward!(tape),  CUDAdrv.synchronize())
-    NVTX.@range "backward pass" (backward!(tape), CUDAdrv.synchronize())
+    NVTX.@range "forward pass"  (forward!(TAPE),  CUDAdrv.synchronize())
+    NVTX.@range "backward pass" (backward!(TAPE), CUDAdrv.synchronize())
 end
 
-benchmark(tape) # warm-up
+benchmark(TAPE) # warm-up
 
 NVTX.@activate CUDAdrv.@profile begin
     ccall(:jl_dump_compiles, Cvoid, (Ptr{Cvoid},), STDERR.handle)
-    benchmark(tape)
+    benchmark(TAPE)
     ccall(:jl_dump_compiles, Cvoid, (Ptr{Cvoid},), C_NULL)
 end
