30cc73ac451716e5576b84fc1103e9fff82d08a5
Tim Besard <tim.besard@gmail.com>
2018-02-15 16:21:44 -0500

    Benchmarking changes to Python script.

--- test/perf/hmlstm.py
+++ test/perf/hmlstm.py
@@ -1,10 +1,13 @@
 import argparse
 import tensorflow as tf
-import time
+from tensorflow.python.client import timeline
+import timeit
+import pycuda
+import pycuda.autoinit
 
 parser = argparse.ArgumentParser()
-parser.add_argument('dims', default=2048, type=int, help='dimension of differentiable variables')
-parser.add_argument('device', default="/device:GPU:0", type=str, help='which device should be used to execute the benchmark')
+parser.add_argument('dims', nargs='?', default=2048, type=int, help='dimension of differentiable variables')
+parser.add_argument('device', nargs='?', default="/device:GPU:0", type=str, help='which device should be used to execute the benchmark')
 
 # The below code is a snippet adapted from the
 # canonical HMLSTM implementation, specifically
@@ -26,31 +29,71 @@ def hmlstm_update_c(z, zb, c, f, i, g):
         )
     )
 
-def benchmark(dims, device_name):
-    # set up control variables
-    z = tf.Variable(tf.cast(tf.less(tf.random_uniform([dims]), 0.5), dtype=tf.float32), name='z')
-    zb = tf.Variable(tf.cast(tf.less(tf.random_uniform([dims]), 0.5), dtype=tf.float32), name='zb')
+class Benchmark:
+    def __init__(self, dims):
+        # set up control variables
+        z = tf.Variable(tf.cast(tf.less(tf.random_uniform([dims]), 0.5), dtype=tf.float32), name='z')
+        zb = tf.Variable(tf.cast(tf.less(tf.random_uniform([dims]), 0.5), dtype=tf.float32), name='zb')
 
-    # set up differentiable variables
-    c = tf.Variable(tf.random_uniform([dims, dims]), name='c')
-    f = tf.Variable(tf.random_uniform([dims, dims]), name='f')
-    i = tf.Variable(tf.random_uniform([dims, dims]), name='i')
-    g = tf.Variable(tf.random_uniform([dims, dims]), name='g')
+        # set up differentiable variables
+        c = tf.Variable(tf.random_uniform([dims, dims]), name='c')
+        f = tf.Variable(tf.random_uniform([dims, dims]), name='f')
+        i = tf.Variable(tf.random_uniform([dims, dims]), name='i')
+        g = tf.Variable(tf.random_uniform([dims, dims]), name='g')
 
-    # build the computation graph for our kernel
-    new_c = hmlstm_update_c(z, zb, c, f, i, g)
-    c_grad, f_grad, i_grad, g_grad = tf.gradients(new_c, [c, f, i, g])
+        # build the computation graph for our kernel
+        new_c = hmlstm_update_c(z, zb, c, f, i, g)
+        self.c_grad, self.f_grad, self.i_grad, self.g_grad = tf.gradients(new_c, [c, f, i, g])
 
-    # execute the benchmark
-    with tf.Session() as sess:
-        with tf.device(device_name):
-            sess.run(tf.global_variables_initializer())
-            start_time = time.clock()
-            sess.run([c_grad, f_grad, i_grad, g_grad])
-            elapsed_time = time.clock() - start_time
+    def warmup(self, sess):
+        sess.run(tf.global_variables_initializer())
+        self.run(sess)
 
-    return elapsed_time
+    def run(self, sess, **kwargs):
+        sess.run([self.c_grad, self.f_grad, self.i_grad, self.g_grad], **kwargs)
+
+def pretty_print_time(s):
+    if s < 1e-6:
+        unit = "n"
+        factor = 1e9
+    elif s < 1e-3:
+        unit = "u"
+        factor = 1e6
+    elif s < 1:
+        unit = "m"
+        factor = 1e3
+    else:
+        unit = ""
+        factor = 1
+    print("{0:.2f} {1}s".format(s*factor, unit))
+
+# turn on the XLA JIT compiler
+config = tf.ConfigProto()
+config.graph_options.optimizer_options.global_jit_level = tf.OptimizerOptions.ON_1
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    print(benchmark(args.dims, args.device))
+    b = Benchmark(args.dims)
+    with tf.Session(config=config) as sess:
+        with tf.device(args.device):
+            b.warmup(sess)
+
+            t = timeit.Timer("b.run(sess)", globals=globals())
+            its, total_time = t.autorange()
+            pretty_print_time(total_time / its)
+
+            pycuda.driver.start_profiler()
+            b.run(sess)
+            pycuda.driver.stop_profiler()
+
+            # alternatively, use TF's built-in tracer
+            # (this breaks nvprof as it uses CUPTI too, hence commented out)
+
+            # options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+            # run_metadata = tf.RunMetadata()
+            # b.run(sess, options=options, run_metadata=run_metadata)
+
+            # fetched_timeline = timeline.Timeline(run_metadata.step_stats)
+            # chrome_trace = fetched_timeline.generate_chrome_trace_format()
+            # with open('hmlstm.json', 'w') as f:
+            #     f.write(chrome_trace)
