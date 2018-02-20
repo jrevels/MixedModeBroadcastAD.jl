b99d882fb7324cdf0cf5ac4cd5fdd8cf814b0568
Jarrett Revels <jarrettrevels@gmail.com>
2018-02-20 11:28:03 -0500

    set up control values to hit all cases

--- test/kernels.jl
+++ test/kernels.jl
@@ -38,7 +38,10 @@ end
 ###################
 
 function getkernel(kind::Symbol, dims::Int = 2048)
+    @assert dims >= 3
     @assert kind == :cpu || kind == :gpu
+
+    # select kernel/array kind
     if kind == :cpu
         kernel = cpu_hmlstm_update_c
         A = Array
@@ -46,10 +49,21 @@ function getkernel(kind::Symbol, dims::Int = 2048)
         kernel = gpu_hmlstm_update_c
         A = CuArray
     end
-    control = (convert(A, round.(rand(Float32, dims))) for _ in 1:2)
-    compute = (convert(A, rand(Float32, dims, dims)) for _ in 1:4)
-    input_values = (control..., compute...)
+
+    # set up control variables to ensure that we will hit all three cases at least once
+    control = (round.(rand(Float32, dims)), round.(rand(Float32, dims)))
+    control[1][1] = 1.0f0 # FLUSH case
+    control[2][1] = 0.0f0
+    control[1][2] = 0.0f0 # COPY case
+    control[2][2] = 0.0f0
+    control[1][3] = 0.0f0 # UPDATE case
+    control[2][3] = 1.0f0
+    control = (convert(A, control[1]), convert(A, control[2]))
+
+    # set up the rest of our values/buffers
+    input_values = (control..., (convert(A, rand(Float32, dims, dims)) for _ in 1:4)...)
     input_derivs = similar.(input_values)
     output_value = rand(Float32, dims, dims)
+
     return kernel, input_values, input_derivs, output_value
 end
