66c44784da6455cc7dcc5c927cd23db75d239492
Tim Besard <tim.besard@gmail.com>
2018-03-06 08:29:33 +0100

    Process all collected data.

--- test/process.jl
+++ test/process.jl
@@ -29,11 +29,7 @@ function Base.show(io::IO, it::Iteration)
 end
 
 info("#26278 workaround") # yeah no kiddin' printing something makes it work. fml
-function process_data(path)
-    table = open(path, "r") do io
-        deserialize(io)
-    end
-
+function process_data(table)
     its = Iteration[]
     it = nothing 
     it_start = 0.
@@ -74,24 +70,70 @@ function process_data(path)
     return its
 end
 
-@assert length(ARGS) >= 1
-const INPUT = ARGS[1]
+function average_data(its)
+    # NOTE: better just chuck everything in the DataFrame,
+    #       and group+average when displaying
 
-its = process_data(INPUT)
-popfirst!(its)
+    popfirst!(its)
 
-avg_it = Iteration{Measurement{Float64}}()
+    avg_it = Iteration{Measurement{Float64}}()
 
-# counters should be identical
-for field in (:kernel_count, :memcpy_count, :api_count)
-    @assert all(it->getfield(its[1], field) == getfield(it, field), its)
-    setfield!(avg_it, field, getfield(its[1], field))
+    # counters should be identical
+    for field in (:kernel_count, :memcpy_count, :api_count)
+        @assert all(it->getfield(its[1], field) == getfield(it, field), its)
+        setfield!(avg_it, field, getfield(its[1], field))
+    end
+
+    # average timings
+    for field in (:total_time, :kernel_time, :memcpy_time, :api_time)
+        val = mean([getfield(it, field) for it in its]) ± std([getfield(it, field) for it in its])
+        setfield!(avg_it, field, val)
+    end
+
+    return avg_it
 end
 
-# average timings
-for field in (:total_time, :kernel_time, :memcpy_time, :api_time)
-    val = mean([getfield(it, field) for it in its]) ± std([getfield(it, field) for it in its])
-    setfield!(avg_it, field, val)
+function process(path)
+    df = open(path, "r") do io
+        deserialize(io)
+    end
+
+    its = process_data(df)
+    return average_data(its)
 end
 
-println(avg_it)
+function add_row!(df, it; system, TFstyle=missing, arity=missing, dims)
+    push!(df, [system, TFstyle, arity, dims,
+               it.total_time,
+               it.kernel_count, it.kernel_time,
+               it.memcpy_count, it.memcpy_time,
+               it.api_count, it.api_time])
+end
+
+df = DataFrame(system = Symbol[], TFstyle=Union{Missing,Bool}[], arity=Union{Missing,Int}[],
+               dims=Int[],
+               total_time = AbstractFloat[],
+               kernel_count = Int[], kernel_time = AbstractFloat[],
+               memcpy_count = Int[], memcpy_time = AbstractFloat[],
+               api_count = Int[], api_time = AbstractFloat[])
+
+cd(@__DIR__) do
+    for dims in [512,1024,2048]
+        let
+            it = process("python_$(dims).jls")
+            add_row!(df, it; dims=dims, system=:python)
+        end
+
+        for tfstyle in [true, false]
+            it = process("julia_$(tfstyle ? "tf_" : "")$(dims).jls")
+            add_row!(df, it; dims=dims, system=:julia, TFstyle=tfstyle)
+        end
+
+        for arity in 1:10
+            it = process("julia_arity$(arity)_$(dims).jls")
+            add_row!(df, it; dims=dims, system=:julia, arity=arity)
+        end
+    end
+end
+
+println(df)
