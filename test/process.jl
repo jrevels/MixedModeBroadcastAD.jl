1712b86db90be695f250493b7eac6f6a1baac485
Tim Besard <tim.besard@gmail.com>
2018-04-27 14:31:27 +0200

    Organize according to #34

--- test/process.jl
+++ test/process.jl
@@ -7,34 +7,34 @@ using DataStructures
 mutable struct Kernel{T<:AbstractFloat}
     name::String
     registers::Int
-    duration::T
+    runtime::T
 end
-duration(kernel::Kernel) = kernel.duration
+runtime(kernel::Kernel) = kernel.runtime
 registers(kernel::Kernel) = kernel.registers
 
 mutable struct Iteration{T<:AbstractFloat}
-    duration::T
+    runtime::T
 
     # kernels
     kernels::Vector{Kernel{T}}
 
-    # memory operations
-    memcpy_size::Float64
-    memcpy_count::Int
-    memcpy_duration::T
+    # memory transfers
+    transfer_size::Float64
+    transfer_count::Int
+    transfer_runtime::T
 
     # API calls
     api_count::Int
-    api_duration::T
+    api_runtime::T
 
     Iteration{T}() where {T} = new{T}(zero(T), Kernel{T}[], 0.0, 0, zero(T), 0, zero(T))
 end
 
 function Base.show(io::IO, it::Iteration)
-    println(io, "Iteration taking $(round(it.duration,2)) us:")
-    println(io, " - $(length(it.kernels)) kernel launches: $(round(sum(duration, it.kernels), 2)) us")
-    println(io, " - $(it.memcpy_count) memory copies: $(round(it.memcpy_size, 2)) MB in $(round(it.memcpy_duration, 2)) us")
-    print(io, " - $(it.api_count) API calls: $(round(it.api_duration, 2)) us")
+    println(io, "Iteration taking $(round(it.runtime,2)) us:")
+    println(io, " - $(length(it.kernels)) kernel launches: $(round(sum(runtime, it.kernels), 2)) us")
+    println(io, " - $(it.transfer_count) memory transfers: $(round(it.transfer_size, 2)) MB in $(round(it.transfer_runtime, 2)) us")
+    print(io, " - $(it.api_count) API calls: $(round(it.api_runtime, 2)) us")
 end
 
 function group_trace(table)
@@ -52,7 +52,7 @@ function group_trace(table)
         @assert it != nothing
         if contains(row[:Name], "[Range end]")
             it_stop = row[:Start]
-            it.duration = it_stop - it_start
+            it.runtime = it_stop - it_start
             push!(its, it)
             it = nothing
             continue
@@ -64,12 +64,12 @@ function group_trace(table)
             # TODO: only count API calls that contribute to the total execution (ie. no
             # async calls, event queries, etc)
             if row[:Name] != "cuCtxSynchronize"
-                it.api_duration += row[:Duration]
+                it.api_runtime += row[:Duration]
             end
         elseif contains(row[:Name], "[CUDA memcpy")
-            it.memcpy_size += row[:Size]
-            it.memcpy_count += 1
-            it.memcpy_duration += row[:Duration]
+            it.transfer_size += row[:Size]
+            it.transfer_count += 1
+            it.transfer_runtime += row[:Duration]
         else
             push!(it.kernels, Kernel{Float64}(row[:Name], row[:Registers_Per_Thread], row[:Duration]))
         end
@@ -95,20 +95,20 @@ function average_trace(its)
             Kernel{Measurement{Float64}}(
                 its[1].kernels[i].name,
                 its[1].kernels[i].registers,
-                mean([it.kernels[i].duration for it in its]) ±
-                 std([it.kernels[i].duration for it in its]),
+                mean([it.kernels[i].runtime for it in its]) ±
+                 std([it.kernels[i].runtime for it in its]),
             )
         )
     end
 
     # other
     ## counters
-    for field in (:memcpy_size, :memcpy_count, :api_count)
+    for field in (:transfer_size, :transfer_count, :api_count)
         @assert all(it->getfield(its[1], field) == getfield(it, field), its)
         setfield!(avg_it, field, getfield(its[1], field))
     end
     ## timings
-    for field in (:duration, :memcpy_duration, :api_duration)
+    for field in (:runtime, :transfer_runtime, :api_runtime)
         val = mean([getfield(it, field) for it in its]) ± std([getfield(it, field) for it in its])
         setfield!(avg_it, field, val)
     end
@@ -145,79 +145,76 @@ end
 
 # basic timings
 
-function add_row!(df, trace, metrics; system, TFstyle=missing, arity=missing, dims)
-    push!(df, [system, TFstyle, arity, dims,
-               trace.duration,
+function add_row!(df, trace, metrics; implementation, arity=missing, problem_size)
+    push!(df, [implementation, arity, problem_size,
+               trace.runtime,
                length(trace.kernels), maximum(registers.(trace.kernels)),
-               sum(duration, trace.kernels),
+               sum(runtime, trace.kernels),
                mean(kernel->get_metric(metrics, kernel.name, "achieved_occupancy")[:Avg],
                     trace.kernels),
-               trace.memcpy_size, trace.memcpy_count, trace.memcpy_duration,
-               trace.api_count, trace.api_duration])
+               trace.transfer_size, trace.transfer_count, trace.transfer_runtime,
+               trace.api_count, trace.api_runtime])
 end
 
 function process(dir)
-    df = DataFrame(system = Symbol[], TFstyle=Union{Missing,Bool}[], arity=Union{Missing,Int}[],
-                   dims=Int[],
-                   duration = AbstractFloat[],
+    df = DataFrame(implementation = Symbol[], arity=Union{Missing,Int}[],
+                   problem_size=Int[],
+                   runtime = AbstractFloat[],
                    kernel_count = Int[], kernel_registers = Int[],
-                   kernel_duration = AbstractFloat[],  kernel_occupancy = AbstractFloat[],
-                   memcpy_size = Float64[], memcpy_count = Int[], memcpy_duration = AbstractFloat[],
-                   api_count = Int[], api_duration = AbstractFloat[])
+                   kernel_runtime = AbstractFloat[],  kernel_occupancy = AbstractFloat[],
+                   transfer_size = Float64[], transfer_count = Int[], transfer_runtime = AbstractFloat[],
+                   api_count = Int[], api_runtime = AbstractFloat[])
 
     cd(dir) do
-        for dims in [512,1024,2048]
+        for problem_size in [512,1024,2048]
             let
-                metrics, trace = read("python_$(dims)")
-                add_row!(df, trace, metrics; dims=dims, system=:python)
+                metrics, trace = read("python_$(problem_size)")
+                add_row!(df, trace, metrics; problem_size=problem_size, implementation=:python)
             end
 
             for tfstyle in [true, false]
-                metrics, trace = read("julia_$(tfstyle ? "tf_" : "")$(dims)")
-                add_row!(df, trace, metrics; dims=dims, system=:julia, TFstyle=tfstyle)
+                metrics, trace = read("julia_$(tfstyle ? "tf_" : "")$(problem_size)")
+                add_row!(df, trace, metrics; problem_size=problem_size, implementation=(tfstyle ? :julia_tfstyle : :julia))
             end
 
             for arity in 1:10
-                metrics, trace = read("julia_arity$(arity)_$(dims)")
-                add_row!(df, trace, metrics; dims=dims, system=:julia, arity=arity)
+                metrics, trace = read("julia_arity$(arity)_$(problem_size)")
+                add_row!(df, trace, metrics; problem_size=problem_size, implementation=:julia, arity=arity)
             end
         end
     end
 
-    let df = df
-        df = df[.!ismissing.(df[:arity]),:]
-        df = df[[:arity, :dims, :kernel_registers, :kernel_duration, :kernel_occupancy]]
+    function prepare_properties!(df)
+        df[:runtime_err] = Measurements.uncertainty.(df[:runtime])
+        df[:runtime] = Measurements.value.(df[:runtime])
 
-        df[:duration_val] = Measurements.value.(df[:kernel_duration])
-        df[:duration_err] = Measurements.uncertainty.(df[:kernel_duration])
-        delete!(df, :kernel_duration)
+        df[:kernel_runtime_err] = Measurements.uncertainty.(df[:kernel_runtime])
+        df[:kernel_runtime] = Measurements.value.(df[:kernel_runtime])
 
-        rename!(df, :kernel_registers, :registers)
+        df[:transfer_runtime_err] = Measurements.uncertainty.(df[:transfer_runtime])
+        df[:transfer_runtime] = Measurements.value.(df[:transfer_runtime])
 
-        rename!(df, :kernel_occupancy, :occupancy)
+        df[:api_runtime_err] = Measurements.uncertainty.(df[:api_runtime])
+        df[:api_runtime] = Measurements.value.(df[:api_runtime])
+    end
+
+    let df = df[ismissing.(df[:arity]),:]
+        delete!(df, :arity)
+        prepare_properties!(df)
+
+        display(df)
+
+        writetable(joinpath(dirname(@__DIR__), "img", "hmlstm.csv"), df)
+    end
+
+    let df = df[.!ismissing.(df[:arity]),:]
+        delete!(df, :implementation)
+        prepare_properties!(df)
 
         writetable(joinpath(dirname(@__DIR__), "img", "arity.csv"), df)
     end
 
-    let df = df
-        df = df[ismissing.(df[:arity]),:]
-        df = df[[:system, :TFstyle, :dims, :kernel_duration, :memcpy_duration]]
-
-        df[(df[:system] .== :julia) .& (df[:TFstyle] .== true), :system] = :julia_tfstyle
-        delete!(df, :TFstyle)
-
-        df[:compute_val] = Measurements.value.(df[:kernel_duration])
-        df[:compute_err] = Measurements.uncertainty.(df[:kernel_duration])
-        delete!(df, :kernel_duration)
-
-        df[:memory_val] = Measurements.value.(df[:memcpy_duration])
-        df[:memory_err] = Measurements.uncertainty.(df[:memcpy_duration])
-        delete!(df, :memcpy_duration)
-
-        writetable(joinpath(dirname(@__DIR__), "img", "compute.csv"), df)
-    end
-
-    df
+    nothing
 end
 
 dir = if length(ARGS) >= 1
