aefeb1664220b58473b76edfe45331e7b2722b0c
Tim Besard <tim.besard@gmail.com>
2018-05-09 14:27:28 +0200

    Add wrapper for collect.jl that processes each device.

---
+++ test/collect_all.jl
@@ -0,0 +1,20 @@
+#!/usr/bin/env julia
+
+using CUDAdrv
+
+const gpus = Dict{String,Int}()
+for (i,device) in enumerate(devices())
+    id = name(device)
+    get!(gpus, id, i-1)
+end
+
+const ITERATIONS = length(ARGS) >= 1 ? parse(Int, ARGS[1]) : 128
+
+cd(@__DIR__) do
+    for (gpu,i) in gpus
+        @info "Collecting data for $gpu (device $i)"
+        withenv("CUDA_VISIBLE_DEVICES" => i) do
+            run(`julia --depwarn=no --compiled-modules=no collect.jl $ITERATIONS $gpu`)
+        end
+    end
+end
